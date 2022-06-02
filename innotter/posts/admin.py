83cf3a14a9da86df1de45223499b9653c98c537b
astivard <maksim.dravitsa@innowise-group.com>
2022-06-02 13:14:37 +0300

    Configure admin-panel
    Set up admin.py in each app

--- innotter/posts/admin.py
+++ innotter/posts/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from posts.models import Post
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'page', 'reply_to', 'updated_at')
+    list_display_links = ('id', 'page')
+
+
+admin.site.register(Post, PostAdmin)
--- innotter/users/admin.py
+++ innotter/users/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    list_display = ('id', 'username', 'title', 'email', 'role', 'is_blocked')
+    list_display_links = ('id', 'username')
+
+
+admin.site.register(User, CustomUserAdmin)
